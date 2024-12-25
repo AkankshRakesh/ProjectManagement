8925f03f9a4360d229b8d2994754c69de5ca74a9
AkankshRakesh <akankshrakesh@gmail.com>
2024-12-25 22:14:08 +0530

    3:17:56

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Header from '@/components/Header';
+import React, { useState } from 'react'
 
-const ProjectHeader = () => {
+type Props = {
+  activeTab: string;
+  setActiveTab: (tabName: string) => void;
+}
+
+const ProjectHeader = ({activeTab, setActiveTab}: Props) => {
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false)
   return (
-    <div>ProjectHeader</div>
+    <div className='px-4 xl:px-6'>
+      <div className='pb-6 pt-6 lg:pb-4 lg:pt-8'>
+        <Header name="Product Design and Development" />
+      </div>
+    </div>
   )
 }
 
