0276ba9f474239fb0995d6938cd997dd70a6508a
AkankshRakesh <akankshrakesh@gmail.com>
2024-12-27 14:13:20 +0530

    stable commit

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,21 +1,33 @@
 "use client";
-import React, { useState } from 'react'
+import React, { use, useState } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader";
-import Board from '../BoardView';
-type Props = {
-    params: {id : string};
-}
+import Board from "../BoardView";
+import List from "../ListView";
+import Timeline from "../TimelineView";
 
-const page = ({params}: Props) => {
-  const { id } = params;
-  const [activeTab, setActiveTab] = useState('Board');
+type Props = {
+  params: Promise<{ id: string }>; // params is now a Promise
+};
+
+const Page = ({ params }: Props) => {
+  const { id } = use(params); // Use React.use() to unwrap params
+  const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
   return (
     <div>
-        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-        { activeTab === 'Board' && <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/> }
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
